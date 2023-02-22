aafd0c50f5e03ba4c2ed808a55a51e30b4e31d2b
mini1385 <mini1385@innotree.com>
2023-02-22 11:18:06 +0900

    edit ice cream

--- SomeProject/SomeProject/main.swift
+++ SomeProject/SomeProject/main.swift
@@ -1,5 +1,5 @@
 //
-//  main.swift
+//  Icecream.swift
 //  SomeProject
 //
 //  Created by eve on 2023/02/21.
@@ -7,21 +7,27 @@
 
 import Foundation
 
-var row = 11
+var width = 11
 
-func makeIcecream(column: Int) {
-    for _ in 0...column-1 {
-        for _ in 1...row {
+/// 아이스크림 그리기
+func makeIcecream(height: UInt) {
+    if (width < 1 || height < 1) { return }
+    
+    for _ in 0...height-1 {
+        for _ in 0...width-1 {
             print("*", terminator: "")
         }
         print("")
     }
 }
 
-func makeBar(column: Int) {
-    for _ in 0...column-1 {
-        for count in 0...row-1 {
-            if(count == (row/2) - 1 || count == (row/2) + 1) {
+/// 막대 바 그리기
+func makeBar(height: UInt) {
+    if (width < 1 || height < 1) { return }
+    
+    for _ in 0...height-1 {
+        for count in 0...width-1 {
+            if(count == (width/2) - 1 || count == (width/2) + 1) {
                 print("|", terminator: "")
             } else {
                 print(" ", terminator: "")
@@ -31,6 +37,8 @@ func makeBar(column: Int) {
     }
 }
 
+
 /* 아이스크림 만들기 */
-makeIcecream(column: 8)
-makeBar(column: 4)
+makeIcecream(height: 8)
+makeBar(height: 4)
+print("")
